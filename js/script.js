d14df753d55be1a95f8b438872217b2326a89084
marcinWS <marcinws@gmail.com>
2020-07-08 16:56:22 -0400

    ADD @script.js-draw 1-3 & print let

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
 
-printMessage('Zagrałem ' + computerMove + '!Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+//printMessage('Wylosowana liczba to: ' + randomNumber);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+  computerMove = 'papier';
+} else if (randomNumber == 3){
+    computerMove = 'nozyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
