c5a31e4bd6fe8968038d45a3a261919780313c53
marcinWS <marcinws@gmail.com>
2020-07-09 07:47:25 -0400

    ADD @script.js @function displayResult (argComputerMove ==  argPlayerMove)

--- js/script.js
+++ js/script.js
@@ -15,24 +15,26 @@ function getMoveName(argMoveId){
 
   function displayResult(argComputerMove, argPlayerMove){
     console.log('moves:', argComputerMove, argPlayerMove);
-    if(argComputerMove == 'papier' && argPlayerMove == 'papier'){
+    if(argComputerMove ==  argPlayerMove){
         printMessage('REMIS');
+    //if(argComputerMove == 'papier' && argPlayerMove == 'papier'){
+    //    printMessage('REMIS');
     } else if (argComputerMove == 'papier' && argPlayerMove == 'nozyce'){
         printMessage('Ty wygrywasz');
     } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
         printMessage('Wygrywa Komputer');
     } else if (argComputerMove == 'nozyce' && argPlayerMove == 'papier'){
         printMessage('Wygrywa Komputer');
-    } else if (argComputerMove == 'nozyce' && argPlayerMove == 'nozyce'){
-        printMessage('REMIS');
+    //} else if (argComputerMove == 'nozyce' && argPlayerMove == 'nozyce'){
+    //   printMessage('REMIS');
     } else if (argComputerMove == 'nozyce' && argPlayerMove == 'kamień'){
         printMessage('Ty wygrywasz');
     }else if (argComputerMove == 'kamień' && argPlayerMove == 'papier'){
         printMessage('Ty wygrywasz');
     } else if (argComputerMove == 'kamień' && argPlayerMove == 'nozyce'){
         printMessage('Wygrywa Komputer');
-    } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
-        printMessage('REMIS');
+    //} else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+    //    printMessage('REMIS');
     } else if (argPlayerMove != 1 || argPlayerMove != 2 || argPlayerMove != 3 ){
         printMessage('WPROWADZ LICZBE CAŁKOWITĄ W ZAKRESIE 1-3');
     }
