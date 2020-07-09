46f69ca4165e68d5850395163fcec2a60589bde4
marcinWS <marcinws@gmail.com>
2020-07-09 08:42:03 -0400

    ADD @script.js // LISTENER

--- js/script.js
+++ js/script.js
@@ -61,6 +61,16 @@ function playGame(playerInput){
     displayResult(argComputerMove, argPlayerMove);
 
 }
+//LISTENER
 
+document.getElementById('play-rock').addEventListener('click', function(){
+   playGame(1);
+ });
+ document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+  });
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });
 
 
